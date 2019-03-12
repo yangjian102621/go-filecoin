0598348308df53622464e411a5a723c6aab28b1e
Rose Karr <rose@rosalinekarr.com>
2019-03-12 10:53:30 -0400

    Removed new Ping references in node PingMiner

--- node/node.go
+++ node/node.go
@@ -479,7 +479,7 @@ func (node *Node) Start(ctx context.Context) error {
 	}
 	node.HelloSvc = hello.New(node.Host(), node.ChainReader.GenesisCid(), syncCallBack, node.ChainReader.Head, node.Repo.Config().Net, flags.Commit)
 
-	cni := storage.NewClientNodeImpl(dag.NewDAGService(node.BlockService()), node.Host(), node.Ping, node.GetBlockTime())
+	cni := storage.NewClientNodeImpl(dag.NewDAGService(node.BlockService()), node.Host(), node.PorcelainAPI, node.GetBlockTime())
 	var err error
 	node.StorageMinerClient, err = storage.NewClient(cni, node.PorcelainAPI)
 	if err != nil {
